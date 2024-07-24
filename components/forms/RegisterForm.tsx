5d647bb1094093ea340eec9cdda4f0d27b1bf75a
Moniqchege <quichege33@gmail.com>
2024-07-24 13:29:23 +0300

    commit

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -23,8 +23,8 @@ import { PatientFormValidation } from "@/lib/validation";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-phone-number-input/style.css";
 import CustomFormField, { FormFieldType } from "../CustomFormField";
-import { FileUploader } from "../FileUploader";
 import SubmitButton from "../SubmitButton";
+import { FileUploader } from "../FileUploader";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
